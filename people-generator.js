3dadf0f48dc4a77a027b2a4c7a245c437ab66ed3
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-11 09:16:15 +0100

    add new files

---
+++ people-generator.js
@@ -0,0 +1,42 @@
+import {writeFile} from 'node:fs';
+
+const genders = ['female', 'male'];
+const maleNames = ['John', 'Adam', 'Peter', 'Paul', 'Joseph','Wiktor', 'Stefan', 'Charlie', 'Justin', 'Elton'];
+const femaleNames = ['Kathrin', 'Susan', 'Laura', 'Ivone', 'Ursula', 'Eve', 'Julie', 'Rose', 'Jennifer', 'Ann'];
+const lastNames = ['Doe', 'Coben', 'Franklin', 'Monroe', 'Timberlake', 'Gates', 'Smith','Adams','Kats','Didoo','Park'];
+
+const randChoice =(lenght) => {
+    return (
+        Math.floor(Math.random()*lenght)
+    )
+}
+const randAge = () => {
+    const min = 18
+    const max = 78
+    return (Math.floor(Math.random () * (+max + 1 - +min))+ +min)
+}
+
+const people = [];
+
+for (let i=0; i<lastNames.lenght; i++){
+    const personLastName = lastNames[Math.floor(Math.random()* lastNames.lenght)];
+    const personFemaleFirstName= femaleNames[randChoice(femaleNames.lenhgt)];
+    const personMaleFirtName = maleNames[randChoice(maleNames.lenght)];
+    const personGender = genders[randChoice(genders.length)];
+
+    people.push(
+        {
+            gender: personGender,
+            firstName: (personGender == 'female' ? personFemaleFirstName : personMaleFirtName),
+            lastName: personLastName,
+            age: randAge(),
+        }
+    )
+}
+const data = people;
+
+writeFile('people.json', JSON.stringify(data), (err) =>{
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+
